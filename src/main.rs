87a96a5c081d2b7ee1307a3963a881553e8ce6a2
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-02-06 17:08:20 +0300

    Some manipulations with threads

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use crate::timer::Timer;
 
+use std::thread;
+use std::time::Duration;
+
 pub mod timer;
 
 fn main() {
     let my_timer = Timer::new(String::from("timer"), 0, 20);
-    println!("MA-RTOS {:?}!", my_timer);
+    Timer::start_timer(Box::leak(Box::new(my_timer)));
+
+    thread::sleep(Duration::from_millis(7));
+
+    // println!("MA-RTOS {:?}!", my_timer);
 }
--- src/timer.rs
+++ src/timer.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 /// Type for tick counting.
 type TickType = u32;
 
@@ -13,8 +16,20 @@ pub struct Timer {
 }
 
 impl Timer {
-    /// Creating new timer.
+    /// Creates new timer.
     pub fn new(name: String, now_time: TickType, expired_time: TickType) -> Timer {
         Timer { name, now_time, expired_time }
+    }
+
+    /// Starts timer ticking.
+    /// TODO: this ticking should be added as a privilege task in task manager. Now it is in a separate thread.
+    pub fn start_timer(timer: &'static mut Timer) {
+        thread::spawn(move || {
+            loop {
+                timer.now_time += 1;
+                println!("tick {} from the spawned thread!", timer.now_time);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
     }
 }
