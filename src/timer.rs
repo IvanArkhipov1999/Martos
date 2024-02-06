e601f2a55c5a06894c818da6d0f34b1593dd03f8
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-02-06 15:37:04 +0300

    Some timer fields

--- src/timer.rs
+++ src/timer.rs
@@ -1,7 +1,10 @@
-#[derive(Debug)]
+/// Type for tick counting.
+type TickType = u32;
 
 /// The definition of the timers themselves.
 pub struct Timer {
     /// Name of timer. It may be useful for debugging and users.
-    pub name: String
+    pub name: String,
+    /// Number of ticks, after what timer will be expired.
+    pub expired_time: TickType,
 }
