ac338d4987cccbf247c89420c8234aeffe536b0e
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-02-07 19:09:42 +0300

    Some features for C

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,20 @@
+use std::thread;
+use std::time::Duration;
+use lazy_static::lazy_static;
+use crate::timer::Timer;
+
 pub mod connection;
 pub mod timer;
+
+lazy_static! {
+static ref TIMER: Timer = Timer::new(0, 5, 0.1);
+}
+
+#[no_mangle]
+pub extern "C" fn init_timer() {
+    TIMER.start();
+    println!("Ticks time: {}", TIMER.get_tick_counter());
+    thread::sleep(Duration::from_millis(5));
+    println!("Ticks time: {}", TIMER.get_tick_counter());
+    TIMER.stop();
+}
