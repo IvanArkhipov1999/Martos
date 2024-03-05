3f99fd84d1b71d34df2674d08e744dd1729c90ae
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-03-05 07:55:01 +0300

    Add some functions

--- c-library/xtensa-esp32/src/lib.rs
+++ c-library/xtensa-esp32/src/lib.rs
@@ -1,10 +1,28 @@
+// TODO: maybe all this should be in martos, not in c-library folder
+
 #![no_std]
 
+// use ma_rtos::task_manager::TaskExecutor;
 use ma_rtos::timer::{TickType, Timer};
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn setup_timer() {
+    Timer::setup_timer()
+}
+
+#[no_mangle]
+pub extern "C" fn loop_timer() {
+    Timer::loop_timer()
+}
+
+#[no_mangle]
+pub extern "C" fn stop_condition_timer() -> bool {
+    Timer::stop_condition_timer()
 }
 
 #[no_mangle]
