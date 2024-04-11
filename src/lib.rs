2bc160ae98fe1abbbe3853d27a420e17a0fd2b35
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-04-11 18:09:05 +0300

    Conditional allocation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 // TODO: move this to ports of Martos with conditions
+#[cfg(any(target_arch = "riscv32", target_arch = "xtensa"))]
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
