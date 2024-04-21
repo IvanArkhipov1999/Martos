76b116970ceec6bd226c44789db2c2774ef980a3
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-04-21 20:09:38 +0300

    Link with martos

--- examples/c-examples/xtensa-esp32/src/main.c
+++ examples/c-examples/xtensa-esp32/src/main.c
@@ -1,25 +1,49 @@
 #include <string.h>
+//#include <stdio.h>
+#include <stdbool.h>
 
 extern unsigned int _sbss, _ebss, _sidata, _sdata, _edata;
 
-static volatile int sisyphus = 0;
+extern void add_task(void (*setup_fn)(), void (*loop_fn)(), bool (*stop_condition_fn)());
+extern void start_task_manager();
+
+int counter = 0;
+
+void setup_fn() {
+//    printf("Setup hello world!\n");
+}
+
+void loop_fn() {
+    counter++;
+//    printf("Loop hello world!\n");
+//    printf("counter = %i\n", counter);
+}
+
+bool stop_condition_fn() {
+    if (counter == 50) {
+        return true;
+    }
+
+    return false;
+}
+
 int main( void ) {
-  // Increment a variable.
-  while ( 1 ) {
-    ++sisyphus;
-  }
-  return 0;
+    add_task(setup_fn, loop_fn, stop_condition_fn);
+    start_task_manager();
+
+    // (Should never be reached)
+    return 0;
 }
 
 // Application entry point / startup logic.
 void __attribute__( ( noreturn ) ) call_start_cpu0() {
-  // Clear BSS.
-  memset( &_sbss, 0, ( &_ebss - &_sbss ) * sizeof( _sbss ) );
-  // Copy initialized data.
-  memmove( &_sdata, &_sidata, ( &_edata - &_sdata ) * sizeof( _sdata ) );
+    // Clear BSS.
+    memset( &_sbss, 0, ( &_ebss - &_sbss ) * sizeof( _sbss ) );
+    // Copy initialized data.
+    memmove( &_sdata, &_sidata, ( &_edata - &_sdata ) * sizeof( _sdata ) );
 
-  // Done, branch to main
-  main();
-  // (Should never be reached)
-  while( 1 ) {}
+    // Done, branch to main
+    main();
+    // (Should never be reached)
+    while( 1 ) {}
 }
