7d25bfd4fb5c78a14f91eac1a06920c7017af169
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-05-07 20:52:37 +0300

    Update C library

--- c-library/xtensa-esp32/src/lib.rs
+++ c-library/xtensa-esp32/src/lib.rs
@@ -2,64 +2,8 @@
 
 #![no_std]
 
-use esp32_hal as _;
-use esp32_hal::xtensa_lx_rt::exception::{Context, ExceptionCause};
 use martos::task_manager::TaskManager;
 use martos::timer::{TickType, Timer};
-
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_kernel_exception(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked Kernel Exception: {:?}, {:08x?}", cause, save_frame)
-// }
-//
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_user_exception(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked User Exception: {:?}, {:08x?}", cause, save_frame)
-// }
-//
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_double_exception(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked Double Exception: {:?}, {:08x?}", cause, save_frame)
-// }
-//
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_level_2_interrupt(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked Level 2 Interrupt: {:?}, {:08x?}", cause, save_frame)
-// }
-//
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_level_3_interrupt(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked Level 3 Interrupt: {:?}, {:08x?}", cause, save_frame)
-// }
-//
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_level_4_interrupt(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked Level 4 Interrupt: {:?}, {:08x?}", cause, save_frame)
-// }
-//
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_level_5_interrupt(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked Level 5 Interrupt: {:?}, {:08x?}", cause, save_frame)
-// }
-//
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_level_6_interrupt(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked Level 6 Interrupt: {:?}, {:08x?}", cause, save_frame)
-// }
-//
-// #[no_mangle]
-// #[link_section = ".rwtext"]
-// extern "C" fn __naked_level_7_interrupt(cause: ExceptionCause, save_frame: &Context) {
-//     panic!("Naked Level 7 Interrupt: {:?}, {:08x?}", cause, save_frame)
-// }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
