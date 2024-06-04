7a6f095777761afcd6ec2d0352873ca61d233d8f
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-06-04 18:38:59 +0300

    Some timer init

--- src/ports/xtensa_esp32/hardware_timer.rs
+++ src/ports/xtensa_esp32/hardware_timer.rs
@@ -1,7 +1,20 @@
 use crate::timer::TickType;
+use esp32_hal::timer::TimerGroup;
+use esp32_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*};
 
 /// Esp32 hardware timer setup.
-pub fn setup_hardware_timer() {}
+pub fn setup_hardware_timer() {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut timer00 = timer_group0.timer0;
+
+    timer00.start(500u64.millis());
+    timer00.listen();
+}
 
 /// Esp32 getting hardware tick counter.
 pub fn get_tick_counter() -> TickType {
