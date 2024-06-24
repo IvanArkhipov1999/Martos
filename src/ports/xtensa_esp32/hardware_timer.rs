43b4e1474e0451f55435cb9b174f0590d6f26e36
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2024-06-24 21:08:38 +0300

    Hello world example is compiling

--- src/ports/xtensa_esp32/hardware_timer.rs
+++ src/ports/xtensa_esp32/hardware_timer.rs
@@ -13,8 +13,8 @@ pub fn setup_hardware_timer() {
 
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks, None);
 
-    let mut timer00 = timer_group0.timer0;
-    timer00.load_value(500u64.millis());
+    let timer00 = timer_group0.timer0;
+    let _ = timer00.load_value(500u64.millis());
     timer00.start();
     timer00.listen();
     unsafe {
@@ -28,6 +28,6 @@ pub fn get_tick_counter() -> TickType {
         let timer00 = TIMER00.take().expect("Timer error");
         let tick_counter = timer00.now();
         TIMER00 = Some(timer00);
-        tick_counter
+        tick_counter.ticks()
     }
 }
