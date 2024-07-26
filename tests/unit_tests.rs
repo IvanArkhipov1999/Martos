e8e97e9cb33c065bd1e9c4f2bf467008b4514f78
Azernem <160176623+Azernem@users.noreply.github.com>
2024-07-27 00:37:01 +0500

    Update unit_tests.rs

--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,3 +1,7 @@
+
+// <copyright file="unit_tests.rs" company="NematMusaev">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 #[cfg(test)]
 mod unit_tests {
     use martos::task_manager::TaskManager;
@@ -47,40 +51,51 @@ mod unit_tests {
             50
         );
     }
-
     /// Counter for task for test_one_infinite_task_task_manager.
     static TEST_ONE_INFINITE_TASK_TASK_MANAGER_COUNTER: AtomicU32 = AtomicU32::new(1);
     /// Setup function for task for test_one_infinite_task_task_manager.
+    #[test]
     fn test_one_infinite_task_task_manager_setup_fn() {}
     /// Loop function for task for test_one_infinite_task_task_manager.
+    #[test]
     fn test_one_infinite_task_task_manager_loop_fn() {
         TEST_ONE_INFINITE_TASK_TASK_MANAGER_COUNTER.fetch_add(1, Ordering::Relaxed);
     }
     /// Stop function for task for test_one_infinite_task_task_manager.
-    fn test_one_infinite_task_task_manager_stop_condition_fn() -> bool {
+
+    fn test_one_infinite_task_task_manager_stop_condition_fn() -> bool{
         false
     }
     #[test]
     #[sequential]
     /// Tests if task manager with one infinite task works correctly during some time without panic.
-    fn test_one_infinite_task_task_manager() {
-        TaskManager::add_task(
-            test_one_infinite_task_task_manager_setup_fn,
-            test_one_infinite_task_task_manager_loop_fn,
-            test_one_infinite_task_task_manager_stop_condition_fn,
-        );
-        TaskManager::test_start_task_manager();
-    }
+fn test_task_manager_add_task() {
+    TaskManager::add_task(
+        test_one_infinite_task_task_manager_setup_fn,
+        test_one_infinite_task_task_manager_loop_fn,
+        test_one_infinite_task_task_manager_stop_condition_fn,
+    );
+    
+}
+
+#[test]
+fn test_task_manager_start() {
+    TaskManager::test_start_task_manager();
+    
+}
 
     /// Counter for task for test_two_finite_tasks_task_manager.
     static TEST_TWO_FINITE_TASK_TASK_MANAGER_COUNTER1: AtomicU32 = AtomicU32::new(1);
     /// Setup function for task for test_two_finite_tasks_task_manager.
+    #[test]
     fn test_two_finite_tasks_task_manager_setup_fn1() {}
     /// Loop function for task for test_two_finite_tasks_task_manager.
+    #[test]
     fn test_two_finite_tasks_task_manager_loop_fn1() {
         TEST_TWO_FINITE_TASK_TASK_MANAGER_COUNTER1.fetch_add(1, Ordering::Relaxed);
     }
     /// Stop function for task for test_two_finite_tasks_task_manager.
+
     fn test_two_finite_tasks_task_manager_stop_condition_fn1() -> bool {
         let value = unsafe { TEST_TWO_FINITE_TASK_TASK_MANAGER_COUNTER1.as_ptr().read() };
         if value % 50 == 0 {
@@ -91,8 +106,10 @@ mod unit_tests {
     /// Counter for task for test_two_finite_tasks_task_manager.
     static TEST_TWO_FINITE_TASK_TASK_MANAGER_COUNTER2: AtomicU32 = AtomicU32::new(1);
     /// Setup function for task for test_two_finite_tasks_task_manager.
+    #[test]
     fn test_two_finite_tasks_task_manager_setup_fn2() {}
     /// Loop function for task for test_two_finite_tasks_task_manager.
+    #[test]
     fn test_two_finite_tasks_task_manager_loop_fn2() {
         TEST_TWO_FINITE_TASK_TASK_MANAGER_COUNTER2.fetch_add(1, Ordering::Relaxed);
     }
@@ -104,20 +121,41 @@ mod unit_tests {
         }
         false
     }
+    
     #[test]
     #[sequential]
     /// Tests if task manager with two finite tasks works correctly during some time without panic.
-    fn test_two_finite_tasks_task_manager() {
+    fn test_two_finite_tasks_task1_manager() {
         TaskManager::add_task(
             test_two_finite_tasks_task_manager_setup_fn1,
             test_two_finite_tasks_task_manager_loop_fn1,
             test_two_finite_tasks_task_manager_stop_condition_fn1,
         );
+
+        assert_eq!(
+            unsafe { TEST_TWO_FINITE_TASK_TASK_MANAGER_COUNTER1.as_ptr().read() },
+            50
+        );
+    }
+
+    fn test_two_finite_tasks_task2_manager() {
         TaskManager::add_task(
             test_two_finite_tasks_task_manager_setup_fn2,
             test_two_finite_tasks_task_manager_loop_fn2,
             test_two_finite_tasks_task_manager_stop_condition_fn2,
         );
+
+        assert_eq!(
+            unsafe { TEST_TWO_FINITE_TASK_TASK_MANAGER_COUNTER1.as_ptr().read() },
+            50
+        );
+        assert_eq!(
+            unsafe { TEST_TWO_FINITE_TASK_TASK_MANAGER_COUNTER2.as_ptr().read() },
+            25
+        );
+    }
+
+    fn test_two_finite_tasks_task_manager_start() {
         TaskManager::test_start_task_manager();
 
         assert_eq!(
@@ -133,8 +171,10 @@ mod unit_tests {
     /// Counter for task for test_two_different_tasks_task_manager.
     static TEST_TWO_DIFFERENT_TASK_TASK_MANAGER_COUNTER1: AtomicU32 = AtomicU32::new(1);
     /// Setup function for task for test_two_different_tasks_task_manager.
+    #[test]
     fn test_two_different_tasks_task_manager_setup_fn1() {}
     /// Loop function for task for test_two_different_tasks_task_manager.
+    #[test]
     fn test_two_different_tasks_task_manager_loop_fn1() {
         TEST_TWO_DIFFERENT_TASK_TASK_MANAGER_COUNTER1.fetch_add(1, Ordering::Relaxed);
     }
@@ -153,8 +193,10 @@ mod unit_tests {
     /// Counter for task for test_two_different_tasks_task_manager.
     static TEST_TWO_DIFFERENT_TASK_TASK_MANAGER_COUNTER2: AtomicU32 = AtomicU32::new(1);
     /// Setup function for task for test_two_different_tasks_task_manager.
+    #[test]
     fn test_two_different_tasks_task_manager_setup_fn2() {}
     /// Loop function for task for test_two_different_tasks_task_manager.
+    #[test]
     fn test_two_different_tasks_task_manager_loop_fn2() {
         TEST_TWO_DIFFERENT_TASK_TASK_MANAGER_COUNTER2.fetch_add(1, Ordering::Relaxed);
     }
@@ -191,8 +233,10 @@ mod unit_tests {
     /// Counter for task for test_two_infinite_tasks_task_manager.
     static TEST_TWO_INFINITE_TASK_TASK_MANAGER_COUNTER1: AtomicU32 = AtomicU32::new(1);
     /// Setup function for task for test_two_infinite_tasks_task_manager.
+    #[test]
     fn test_two_infinite_tasks_task_manager_setup_fn1() {}
     /// Loop function for task for test_two_infinite_tasks_task_manager.
+    #[test]
     fn test_two_infinite_tasks_task_manager_loop_fn1() {
         TEST_TWO_INFINITE_TASK_TASK_MANAGER_COUNTER1.fetch_add(1, Ordering::Relaxed);
     }
@@ -203,8 +247,10 @@ mod unit_tests {
     /// Counter for task for test_two_infinite_tasks_task_manager.
     static TEST_TWO_INFINITE_TASK_TASK_MANAGER_COUNTER2: AtomicU32 = AtomicU32::new(1);
     /// Setup function for task for test_two_infinite_tasks_task_manager.
+    #[test]
     fn test_two_infinite_tasks_task_manager_setup_fn2() {}
     /// Loop function for task for test_two_infinite_tasks_task_manager.
+    #[test]
     fn test_two_infinite_tasks_task_manager_loop_fn2() {
         TEST_TWO_INFINITE_TASK_TASK_MANAGER_COUNTER2.fetch_add(1, Ordering::Relaxed);
     }
@@ -221,23 +267,34 @@ mod unit_tests {
             test_two_infinite_tasks_task_manager_loop_fn1,
             test_two_infinite_tasks_task_manager_stop_condition_fn1,
         );
+    }
+    
+    #[test]
+    fn test_two_infinite_tasks_task_manager_adding() {
         TaskManager::add_task(
             test_two_infinite_tasks_task_manager_setup_fn2,
             test_two_infinite_tasks_task_manager_loop_fn2,
             test_two_infinite_tasks_task_manager_stop_condition_fn2,
         );
+    }
+
+    #[test]
+    fn test_two_infinite_tasks_task_manager_start() {
         TaskManager::test_start_task_manager();
     }
 
     /// Counter for task for test_setup_task_manager.
     static TEST_SETUP_TASK_MANAGER_COUNTER: AtomicU32 = AtomicU32::new(1);
     /// Setup function for task for test_setup_task_manager.
+    #[test]
     fn test_setup_task_manager_setup_fn() {
         TEST_SETUP_TASK_MANAGER_COUNTER.store(42, Ordering::Relaxed);
     }
     /// Loop function for task for test_setup_task_manager.
+    #[test]
     fn test_setup_task_manager_loop_fn() {}
     /// Stop function for task for test_setup_task_manager.
+    
     fn test_setup_task_manager_stop_condition_fn() -> bool {
         false
     }
@@ -245,12 +302,21 @@ mod unit_tests {
     #[sequential]
     /// Tests if task manager works correctly with setup function during some time without panic.
     fn test_setup_task_manager() {
+        TaskManager::test_start_task_manager();
+
+        assert_eq!(
+            unsafe { TEST_SETUP_TASK_MANAGER_COUNTER.as_ptr().read() },
+            42
+        );
+    }
+
+    #[test]
+    fn test_setup_task_manager_adding() {
         TaskManager::add_task(
             test_setup_task_manager_setup_fn,
             test_setup_task_manager_loop_fn,
             test_setup_task_manager_stop_condition_fn,
         );
-        TaskManager::test_start_task_manager();
 
         assert_eq!(
             unsafe { TEST_SETUP_TASK_MANAGER_COUNTER.as_ptr().read() },
