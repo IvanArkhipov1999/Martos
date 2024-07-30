f9609be11a587d9c61c167525cc6c439f9887349
pepengu <plotnikovdanya@gmail.com>
2024-07-30 21:40:27 +0300

    Fix

--- src/ports/mips64/memory_manager.rs
+++ src/ports/mips64/memory_manager.rs
@@ -1,4 +1,21 @@
-use core::mem::MaybeUninit;
+use alloc::alloc::{GlobalAlloc, Layout};
+use core::ptr::null_mut;
+
+pub struct Dummy;
+
+unsafe impl GlobalAlloc for Dummy {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        null_mut()
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        panic!("dealloc should be never called")
+    }
+}
+
+
+#[global_allocator]
+static ALLOCATOR: Dummy = Dummy;
 
 /// Heap initialization.
 /// For more information see https://github.com/esp-rs/esp-alloc/.
