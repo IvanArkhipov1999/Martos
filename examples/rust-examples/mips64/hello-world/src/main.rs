5ed19eea714aacf73d6f2401330e6c80cc29457f
pepengu <plotnikovdanya@gmail.com>
2024-09-13 17:58:58 +0300

    Hello world example Fix

--- examples/rust-examples/mips64/hello-world/src/main.rs
+++ examples/rust-examples/mips64/hello-world/src/main.rs
@@ -3,6 +3,7 @@
 
 #[macro_use]
 extern crate cortex_m_rt;
+use core::sync::atomic::{AtomicU32, Ordering};
 use martos::init_system;
 use martos::task_manager::TaskManager;
 
@@ -11,14 +12,22 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+static COUNTER: AtomicU32 = AtomicU32::new(1);
+
 /// Setup function for task to execute.
 fn setup_fn() {}
 
 /// Loop function for task to execute.
-fn loop_fn() {}
+fn loop_fn() {
+    COUNTER.fetch_add(1, Ordering::Relaxed);
+}
 
 /// Stop condition function for task to execute.
 fn stop_condition_fn() -> bool {
+    let value = unsafe { COUNTER.as_ptr().read() };
+    if value % 50 == 0 {
+        return true;
+    }
     return false;
 }
 
