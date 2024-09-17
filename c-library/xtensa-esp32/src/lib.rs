d34e1103f61aaf77baa363e9f000bdbac6a1c44b
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2024-09-17 17:38:31 +0000

    Update lib.rs
    Add path to "c-api.rs".

--- c-library/xtensa-esp32/src/lib.rs
+++ c-library/xtensa-esp32/src/lib.rs
@@ -5,7 +5,7 @@
 use esp_hal as _;
 use martos::task_manager::TaskManager;
 use martos::timer::{TickType, Timer};
-use martos::c-api;
+use martos::src::c-api;
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
