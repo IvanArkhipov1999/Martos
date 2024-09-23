beb60f8347594533c69fd873a50343646b02b611
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2024-09-23 16:13:39 +0000

    add flag #[cfg(feature = "c-library")] in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #![no_std]
+
 mod ports;
 use ports::PortTrait;
 pub mod task_manager;
 pub mod timer;
+#[cfg(feature = "c-library")]
 pub mod c_api;
 
 /// Martos initialization. Should be called before using Martos functions.
