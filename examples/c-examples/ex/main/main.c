f637b86ae310b6aa916a04a8590d8f8e44aeafee
Artem-Nesterenko2005 <hohol.amogus@mail.ru>
2024-12-01 22:06:44 +0300

    undefined reference error

---
+++ examples/c-examples/ex/main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//#include "rust_lib.h"
+extern void init_system(void);
+extern void start_task_manager();
+
+void app_main(void) {
+    init_system();
+    start_task_manager();
+    printf("Hello world from C!\n");
+}
