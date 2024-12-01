19099dc148ac151e4f6cfe5f8df00e4da5802d49
Artem-Nesterenko2005 <hohol.amogus@mail.ru>
2024-12-02 02:40:01 +0300

    need add compatibility with features martos

---
+++ examples/c-examples/ex/components/rust_ex/src/lib.rs
@@ -0,0 +1,10 @@
+#[no_mangle]
+pub extern "C" fn start_task_manager()
+{
+    martos::c_api::start_task_manager();
+}
+#[no_mangle]
+pub extern "C" fn init_system()
+{
+    martos::init_system();
+}
---
+++ examples/c-examples/ex/main/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//extern void init_system(void);
+//extern void start_task_manager();
+
+void app_main(void) {
+    //init_system();
+    //start_task_manager();
+    printf("Hello world from C!\n");
+}
