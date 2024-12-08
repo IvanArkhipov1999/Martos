7a432283b087577dfc18b17fdbe280b45d3501ed
ArtemKhel <ArtemKhel@gmail.com>
2024-12-08 15:30:15 +0300

    increase heap size

--- src/ports/xtensa_esp32/memory_manager.rs
+++ src/ports/xtensa_esp32/memory_manager.rs
@@ -2,7 +2,7 @@ use core::mem::MaybeUninit;
 use esp_alloc as _;
 
 pub(crate) fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
+    const HEAP_SIZE: usize = 64 * 1024;
     static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
 
     unsafe {
