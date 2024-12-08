df2230627e45181269204cc39a0805c1824f825f
Maxim Rodionov <rodionov.maxim05@gmail.com>
2024-12-08 12:47:59 +0300

    Increase heap memory

--- src/ports/xtensa_esp32/memory_manager.rs
+++ src/ports/xtensa_esp32/memory_manager.rs
@@ -4,7 +4,7 @@ use esp_alloc as _;
 /// Heap initialization.
 /// For more information see https://github.com/esp-rs/esp-alloc/.
 pub fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
+    const HEAP_SIZE: usize = 64 * 1024;
     static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
 
     unsafe {
