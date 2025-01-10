b1d12850958a85429ffa404a9034575609251498
arkhipov.iv99@mail.ru <arkhipov.iv99@mail.ru>
2025-01-10 18:20:31 +0300

    Update api

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ pub mod timer;
 #[cfg(any(target_arch = "riscv32", target_arch = "xtensa"))]
 #[cfg(feature = "network")]
 use esp_wifi::esp_now::EspNow;
+#[cfg(target_arch = "xtensa")]
+use esp_hal::{gpio::*, peripherals::*};
 
 /// Martos initialization. Should be called before using Martos functions.
 pub fn init_system() {
@@ -26,4 +28,14 @@ pub fn init_system() {
 #[cfg(feature = "network")]
 pub fn get_esp_now() -> EspNow<'static> {
     return ports::Port::get_esp_now();
+}
+
+#[cfg(target_arch = "xtensa")]
+pub fn get_uart2() -> UART2 {
+    return ports::Port::get_uart2();
+}
+
+#[cfg(target_arch = "xtensa")]
+pub fn get_io() -> Io {
+    return ports::Port::get_io();
 }
