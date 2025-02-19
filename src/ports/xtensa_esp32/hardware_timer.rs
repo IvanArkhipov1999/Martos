ef0b1a4c12afc6791eb0d5b6b135d3cd879e9772
artem <hohol.amogus@mail.ru>
2025-02-20 00:45:58 +0300

    Initializing timers separately from peripherals

--- src/ports/xtensa_esp32/hardware_timer.rs
+++ src/ports/xtensa_esp32/hardware_timer.rs
@@ -4,7 +4,24 @@ use core::time::Duration;
 use esp_hal::timer::timg::{Timer, Timer0, TimerGroup};
 use esp_hal::{peripherals::*, prelude::*};
 
+pub static mut TIMER00: Option<Timer<Timer0<TIMG0>, esp_hal::Blocking>> = None;
+pub static mut TIMER10: Option<Timer<Timer0<TIMG1>, esp_hal::Blocking>> = None;
+
 static TIMER_BUSY: AtomicBool = AtomicBool::new(false);
+
+/// Esp32 hardware timer setup.
+pub fn setup_hardware_timer() {
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0);
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1);
+
+    let timer00 = timer_group0.timer0;
+    let timer10 = timer_group1.timer0;
+
+    unsafe {
+        TIMER00 = Some(timer00);
+        TIMER10 = Some(timer10);
+    }
+}
 
 /// Esp32 attempt to acquire timer.
 pub fn try_acquire_timer() -> bool {
--- src/ports/xtensa_esp32/peripherals.rs
+++ src/ports/xtensa_esp32/peripherals.rs
@@ -1,25 +1,13 @@
-use core::sync::atomic::{AtomicBool, Ordering};
-use core::time::Duration;
-use esp_hal::timer::timg::{Timer, Timer0, TimerGroup};
 use esp_hal::{peripherals::*, prelude::*};
 
-pub static mut TIMER00: Option<Timer<Timer0<TIMG0>, esp_hal::Blocking>> = None;
-pub static mut TIMER10: Option<Timer<Timer0<TIMG1>, esp_hal::Blocking>> = None;
 pub static mut PERIFERALS_RNG: Option<RNG> = None;
 pub static mut PERIFERALS_RADIO_CLK: Option<RADIO_CLK> = None;
 pub static mut PERIFERALS_WIFI: Option<WIFI> = None;
 
 pub fn init_peripherals() {
     let peripherals = esp_hal::init(esp_hal::Config::default());
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0);
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1);
-
-    let timer00 = timer_group0.timer0;
-    let timer10 = timer_group1.timer0;
 
     unsafe {
-        TIMER00 = Some(timer00);
-        TIMER10 = Some(timer10);
         PERIFERALS_RNG = Some(peripherals.RNG);
         PERIFERALS_RADIO_CLK = Some(peripherals.RADIO_CLK);
         PERIFERALS_WIFI = Some(peripherals.WIFI);
