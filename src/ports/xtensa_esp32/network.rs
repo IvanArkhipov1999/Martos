de1ce9a9e4a26d0f4f8ec150c2aecf06c11959a2
artem <hohol.amogus@mail.ru>
2025-02-26 00:41:43 +0300

    Fix fmt

--- src/ports/xtensa_esp32/network.rs
+++ src/ports/xtensa_esp32/network.rs
@@ -24,7 +24,6 @@ pub fn init_network() {
                 Rng::new(peripherals_rng),
                 peripherals_radio_clk,
             )
-
             .unwrap();
 
             ESP_NOW = Some(esp_wifi::esp_now::EspNow::new(&init, peripherals_wifi).unwrap());
