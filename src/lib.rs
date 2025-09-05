8cd85e11ebfff8ec91fceec9f3490759f40fab60
arkhipov.iv99@mail.ru <arkhipov.iv99@mail.ru>
2025-09-05 15:40:47 +0300

    Maybe fix?

--- src/lib.rs
+++ src/lib.rs
@@ -11,41 +11,32 @@ pub mod timer;
 #[cfg(feature = "network")]
 use esp_wifi::esp_now::EspNow;
 
-#[cfg(target_arch = "xtensa")]
-use crate::ports::xtensa_esp32::XtensaEsp32Port as CurrentPort;
-
-#[cfg(target_arch = "mips")]
-use crate::ports::mips64::Mips64 as CurrentPort;
-
-#[cfg(not(any(target_arch = "xtensa", target_arch = "mips")))]
-use crate::ports::mok::Mok as CurrentPort;
-
 /// Martos initialization. Should be called before using Martos functions.
 pub fn init_system() {
     // Memory initialization.
-    CurrentPort::init_heap();
+    ports::Port::init_heap();
     // Hardware timer setup.
-    CurrentPort::setup_hardware_timer();
+    ports::Port::setup_hardware_timer();
     #[cfg(feature = "network")]
     // Network setup.
-    CurrentPort::init_network();
-    // Uart setup.
+    ports::Port::init_network();
+    // Uart
     #[cfg(feature = "uart")]
-    CurrentPort::setup_uart();
+    ports::Port::setup_uart();
 }
 
 #[cfg(any(target_arch = "riscv32", target_arch = "xtensa"))]
 #[cfg(feature = "network")]
 pub fn get_esp_now() -> EspNow<'static> {
-    return CurrentPort::get_esp_now();
+    return ports::Port::get_esp_now();
 }
 
 #[cfg(feature = "uart")]
-pub fn get_uart2() -> <CurrentPort as crate::ports::PortTrait>::Uart2Type {
-    CurrentPort::get_uart2()
+pub fn get_uart2() -> ports::Port::Uart2Type {
+    ports::Port::get_uart2()
 }
 
 #[cfg(feature = "uart")]
-pub fn get_io() -> <CurrentPort as crate::ports::PortTrait>::IoType {
-    CurrentPort::get_io()
+pub fn get_io() -> ports::Port::IoType {
+    ports::Port::get_io()
 }
